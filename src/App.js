a0f37c27ae5ec8c00e7eb9c3d8096c7641b6d505
LukeMcCann <developer-lukemccann@outlook.com>
2021-03-23 21:36:54 +0000

    react-state

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import { Hello } from './components/hello/hello.component';
 
 class App extends Component {
+
+  state = {
+    name: 'Darryl'
+  }
   render() {
     return (
       <div className="App">
-        <Hello name="Darryl"/>
+        <Hello name={this.state.name} />
       </div>
     );
   }
--- src/components/hello/hello.component.jsx
+++ src/components/hello/hello.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './hello.styles.css';
 
 export const Hello = ({ name }) => {
+
     return (
         <h1>Hello {name}</h1>
     );
