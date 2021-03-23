3e7d86fcdc5da33661c36342f1728edb650dfc36
LukeMcCann <developer-lukemccann@outlook.com>
2021-03-23 21:29:30 +0000

    Passing props

--- src/components/hello/hello.component.jsx
+++ src/components/hello/hello.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './hello.styles.css';
 
-export const Hello = (props) => {
+export const Hello = ({ name }) => {
     return (
-        <h1>Hello World</h1>
+        <h1>Hello {name}</h1>
     );
 }
